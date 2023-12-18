b64f7e3f58c027f19c9d0c14ee18ec405e1e29cb
slaclau <77557628+slaclau@users.noreply.github.com>
2023-12-18 12:17:51 +0000

    Small fix

--- src/libantplus/plus/__init__.py
+++ src/libantplus/plus/__init__.py
@@ -1,4 +1,4 @@
 """Extension for ANT+ specific functionality, such as the page based message format.
 
-This module contains an extension of :cls:`libantplus.interface.Interface` and implementations of this for various common sensor types."""
+This module contains an extension of :cls:`libantplus.interface.AntInterface` and implementations of this for various common sensor types."""
 __all__ = ["hrm", "interface", "page"]
